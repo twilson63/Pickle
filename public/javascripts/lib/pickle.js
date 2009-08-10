7feae5a2f26adec3112d786b3f3c75a69be5b07e
twilson63 <twilson63@jackcloud.chn.comcast.net>
2009-08-10 14:28:42 -0400

    consolidated regex into pickle.js

--- public/javascripts/lib/pickle.js
+++ public/javascripts/lib/pickle.js
@@ -276,3 +276,31 @@ Pickle().Step(  {
   }
 });
 
+
+RegExp.escape = function(text) {
+  if (!arguments.callee.sRE) {
+    var specials = [
+      '/', '.', '*', '+', '?', '|',
+      '(', ')', '[', ']', '{', '}', '\\'
+    ];
+    arguments.callee.sRE = new RegExp(
+      '(\\' + specials.join('|\\') + ')', 'g'
+    );
+  }
+  return text.replace(arguments.callee.sRE, '\\$1');
+}
+
+
+RegExp.prototype.escape = function(text) {
+  if (!arguments.callee.sRE) {
+    var specials = [
+      '/', '.', '*', '+', '?', '|',
+      '(', ')', '[', ']', '{', '}', '\\'
+    ];
+    arguments.callee.sRE = new RegExp(
+      '(\\' + specials.join('|\\') + ')', 'g'
+    );
+  }
+  return text.replace(arguments.callee.sRE, '\\$1');
+}            
+
